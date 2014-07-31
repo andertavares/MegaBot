aac55e77f1c6cee1eb57405cfae48fe2b5e4e252
jinsonxu <jinsonxu@gmail.com>
2014-07-31 23:47:05 +0000

    Added method outlines and pseudo code descriptions for TransportManager functionality

--- NUSBot/src/NUSBot/Source/micromanagement/TransportManager.h
+++ NUSBot/src/NUSBot/Source/micromanagement/TransportManager.h
@@ -8,10 +8,19 @@ class MicroManager;
 class TransportManager : public MicroManager
 {
 
+protected:
+	void TransportManager::loadTransportState();
+	BWAPI::Unit * TransportManager::getClosestTransport(BWAPI::Position loadPosition);
+	BOOL transportExists;
+	BOOL techExists;
+
 public:
 
+	/*	Jinson notes: usually safer to have singleton constructors as private or protected, but somehow Squad.h is using it */
 	TransportManager();
 	~TransportManager() {}
-
+	static TransportManager &	Instance();
+	void TransportManager::sendUnits(std::set<BWAPI::Unit *> & unitsToLoad, BWAPI::Position loadPosition, BWAPI::Position unloadPosition);
 	void executeMicro(const UnitVector & targets);
+
 };

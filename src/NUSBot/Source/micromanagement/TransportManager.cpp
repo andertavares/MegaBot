6c8b8d8f922fd013c6a633aa6c1adbc209fdd400
jinsonxu <jinsonxu@gmail.com>
2014-07-31 23:47:05 +0000

    Added method outlines and pseudo code descriptions for TransportManager functionality

--- src/NUSBot/Source/micromanagement/TransportManager.cpp
+++ src/NUSBot/Source/micromanagement/TransportManager.cpp
@@ -1,7 +1,58 @@
+/*	Transport Manager
+ *
+ *	Transport manager handles the building of transport units,
+ *	loading, movement to dropoff location, and unloading.
+ *
+ *	Author: Jinson Xu
+ *	Date: 30th July 2014
+ */
+
+
 #include "Common.h"
 #include "TransportManager.h"
 
-TransportManager::TransportManager()  { }
+
+
+/*	Pseudo Code - Overview of BASIC implementation first
+ *	1. IPBManager will decide which game phase is suitable for drops and then initialize TransportManager
+ *	
+ *	2. TransportManager will check environment for all transport related info and commence required building if no transport is available. 
+ *
+ *	3. IPBManager will invoke public function TransportManager::sendUnits(std::set<BWAPI::Unit *> & unitsToLoad, BWAPI::Position loadPosition, BWAPI::Position unloadPosition)
+ *
+ *	4. TransportManager will direct closest transport unit to loadPosition, load the units, move transport to unloadPosition, unload units, move transport back to loadPosition.
+ */
+
+
+// constructor
+TransportManager::TransportManager()  { 
+	//nothing to initialize in protected constructor for now
+}
+
+
+//get instance of the singleton
+TransportManager & TransportManager::Instance() {
+	static TransportManager instance;
+	return instance;
+}
+
+
+
+void TransportManager::loadTransportState() {
+
+}
+
+
+BWAPI::Unit * TransportManager::getClosestTransport(BWAPI::Position loadPosition) {
+
+}
+
+
+
+void TransportManager::sendUnits(std::set<BWAPI::Unit *> & unitsToLoad, BWAPI::Position loadPosition, BWAPI::Position unloadPosition) {
+
+}
+
 
 void TransportManager::executeMicro(const UnitVector & targets) 
 {

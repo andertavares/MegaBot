8ad601322dbd16bf8c2295f78a0995ec344efbd0
Anderson <andersonrochatavares@gmail.com>
2017-01-13 10:36:00 -0200

    cleanup and better documentation of MetaStrategy

--- MegaBot/src/strategy/MetaStrategy.h
+++ MegaBot/src/strategy/MetaStrategy.h
@@ -9,60 +9,6 @@
 
 using namespace BWAPI;
 using namespace std;
-/*
-struct StrategyStats {
-    string mapHash;
-    string mapName;
-    string strategyId;
-    string ownRace;
-    string opponentRace;
-    int won;
-    int lost;
-    int draw;
-    int total;
-
-    StrategyStats() {
-        won = 0;
-        lost = 0;
-        draw = 0;
-        total = 0;
-    }
-
-    int getTotal() {
-        if (total == 0) return 1; //To avoid division by zero.
-        return total;
-    }
-
-    bool matches() {
-        string mMapHash = Broodwar->mapHash();
-        string mOwnRace = Broodwar->self()->getRace().getName();
-        if (mMapHash == mapHash && mOwnRace == ownRace) {
-            Race oRace = Broodwar->enemy()->getRace();
-            if (oRace.getID() != Races::Unknown.getID()) {
-                //Opponent race is known. Match race as well.
-                if (oRace.getName() == opponentRace) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-};
-
-struct Strategy {
-    Race race;
-    string strategyId;
-
-    Strategy(Race mRace, string mId) {
-        race = mRace;
-        strategyId = mId;
-    }
-};
-*/
 
 /** Base class for meta strategies */
 class MetaStrategy {
@@ -74,21 +20,18 @@ protected:
 	//name of behavior in previous frame
     //string oldBehaviorName;
 
-	//name of current behavior
-	//string strategyName;
-    //string currentStrategy;
-	boost::mt19937 rng; //();	//mersenne twister random number generator
-	//boost::random::uniform_int_distribution<> unifInt; //(0, portfolio.size()); //uniform integer distribution
+	boost::mt19937 rng; //mersenne twister random number generator
 
+	/** Active behavior (corresponds to a bot) */
 	BWAPI::AIModule* currentStrategy;
 
-	//the name of this meta strategy
+	/** Name of this meta strategy */
 	string name;
 
-	//maps the behaviors to their respective names
+	/** Maps behaviors to their respective names */
     std::map<BWAPI::AIModule*, string> strategyNames;
 
-    //maps behavior names to their AIModules
+    /** Maps behavior names to their respectives AIModules */
     std::map<string, BWAPI::AIModule*> portfolio;
 
 	/** Returns a strategy uniformly at random */
@@ -104,22 +47,22 @@ public:
 	static const string XELNAGA;	//"Xelnaga"
 	static const string NUSBot;		//"NUSBot"
 
-	//returns the meta strategy name
+	/** Returns the meta strategy name */
 	string getName();
 
-	//returns the active strategy
+	/** Returns the active strategy */
     BWAPI::AIModule* getCurrentStrategy();
 
-	//returns active behavior name
+	/** Returns active behavior name */
 	string getCurrentStrategyName();
 
-	//acts every frame (may switch strategy or not)
+	/** Acts every frame (may switch strategy or not) */
 	virtual void onFrame() {}
 
-	//initializes the portfolio of behaviors
+	/** Initializes the portfolio of behaviors */
 	virtual void onStart();
 
-	//forces the adoption of a strategy
+	/** Forces the adoption of a strategy */
 	virtual void forceStrategy(string strategyName);
 
     /** Prints debug info to the screen. */
